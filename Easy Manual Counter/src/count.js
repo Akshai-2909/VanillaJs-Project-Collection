912ecfdf527819be3a6232f48d58328532c1166f
Akshai <72917775+Akshai-2909@users.noreply.github.com>
2021-09-27 15:16:05 +0530

    Update count.js

--- Easy Manual Counter/src/count.js
+++ Easy Manual Counter/src/count.js
@@ -1,9 +1,27 @@
-var btnElement = document.getElementById("press");
-var num        = document.getElementById("Number");
-var count = 0;
-btnElement.addEventListener('click', () => {
-   
-    count++;
-    num.textContent =count ;
+var reset = document.querySelector('#reset'); 
+var increase = document.querySelector('#increase');
+var decrease= document.querySelector('#decrease');
+var counts = document.querySelector('#counts');
+let count = 0;
 
+increase.addEventListener('click', ()=>{
+    count++;
+    counts.innerHTML=count;
+});
+
+decrease.addEventListener('click' ,()=>{
+    if(count != 0){
+        count--;
+        counts.innerHTML = count;
+    }
+    
+
+});
+
+reset.addEventListener('click', ()=>{
+    if(count>=0){
+        count = 0;
+        counts.innerHTML = count;
+    }
+    
 });
