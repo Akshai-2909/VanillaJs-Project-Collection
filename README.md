657968e6ae32d6220541d197238bb6d88086c753
Akshai <alphatech2909@gmail.com>
2021-10-05 16:08:05 +0530

    Added To-Do task Manager Project

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # VanillaJs-Project-Collection
-Collection of Basic VanillaJS project with some variety of css implementation created While learning front-end. Below links will direct to codepens.
+Collection of Basic VanillaJS project with some variety of css implementation created While learning front-end. Below are the project codepen links. I sincerely thank to all youtubers and other bloggers who helped to me make this projects.
 
 1. [Easy Manual Counter](https://codepen.io/Ai_J/pen/yLXQErz)
 2. [Background Color Changer](https://codepen.io/Ai_J/pen/powYmdX)
+3. [To-Do Task Manager]
---
+++ To-Do Task Manager/src/app.js
@@ -0,0 +1,96 @@
+//selectors
+const todobar =  document.querySelector('.todo-bar');
+const todobtn = document.querySelector('.todo-btn');
+const todolist = document.querySelector('.todo-list');
+const todocontainer = document.querySelector('.todo-container');
+const filterOption = document.querySelector('.filter-todo');
+//eventlisteners
+
+todobtn.addEventListener('click',addtodo);
+todolist.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click',filtertodo);
+//functions
+function addtodo(event){
+
+    //prevent from submitting
+    event.preventDefault();
+
+    //tododiv
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    //create li
+    const newtodo = document.createElement('li');
+    newtodo.innerText = todobar.value;
+    newtodo.classList.add('todo-item');
+
+    todoDiv.appendChild(newtodo);
+
+    //chech mark button
+    const completedbtn = document.createElement('button');
+    completedbtn.innerHTML= '<i class="fas fa-check"></i>';
+    completedbtn.classList.add('complete-btn');
+    todoDiv.appendChild(completedbtn);
+    
+    //chech trash button
+    const trashbtn = document.createElement('button');
+    trashbtn.innerHTML= '<i class="fas fa-trash"></i>';
+    trashbtn.classList.add('trash-btn');
+    todoDiv.appendChild(trashbtn);
+   
+    //append to dolist
+    todolist.appendChild(todoDiv);
+    //clear todo input
+    todobar.value = "";
+
+}
+
+
+function deleteCheck(e){
+    const item = e.target;
+    //delete todo
+    console.log(item)
+
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        //animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend',function(){
+        todo.remove()
+    });
+    }
+
+    //checkmark
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+
+}
+
+function filtertodo(e){
+    const todos = todolist.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')){
+                todo.style.display = "flex";
+                }
+                else{
+                todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = "flex";
+                }
+                else{
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
+}
